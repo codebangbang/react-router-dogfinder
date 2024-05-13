de368498cc48dc4b38a5cea14d8d11e9540c08ac
ryanfair <ryfair@gmail.com>
2024-05-13 08:28:29 -0400

    restart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Nav from "./Nav";
+import Dogs from "./Dogs";
 
 function App() {
   return (
     <div className="App">
-      <h1>Good luck!</h1>
+      <BrowserRouter>
+      <Nav />
+      <Route exact path="/dogs/:name">
+        <Dogs />
+      </Route>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
